cc8edd802a0e9311fb057414b6d89d97da0f2ed4
Seth Louis <sethvihanlouis@gmail.com>
2023-08-09 13:32:10 -0400

    added rock scene

--- code/proc_gen.gd
+++ code/proc_gen.gd
@@ -19,6 +19,7 @@ const SpikeScene = preload("res://scenes/spikes.tscn")
 const CoinScene = preload("res://scenes/coin.tscn")
 const BarrelScene = preload("res://scenes/barrel.tscn")
 const ChestScene = preload("res://scenes/chest.tscn")
+const RockScene = preload("res://scenes/rocks.tscn")
 
 
 @onready
@@ -66,6 +67,10 @@ func _ready():
 			add_child(obj)
 		for p in get_spawnpnts(i, randi_range(0, 1)):
 			var obj = ChestScene.instantiate()
+			obj.position = Vector3(p.x, 0.05, p.y)
+			add_child(obj)
+		for p in get_spawnpnts(i, randi_range(1, 3)):
+			var obj = RockScene.instantiate()
 			obj.position = Vector3(p.x, 0.05, p.y)
 			add_child(obj)
 
