af01b42476bba19e0139d6a722eee094d08f996a
Seth Louis <sethvihanlouis@gmail.com>
2023-08-11 21:20:32 -0400

    added a fade in/out function
    this function is in the music manager and can be used to fade in or out music

--- code/main_menu.gd
+++ code/main_menu.gd
@@ -4,9 +4,11 @@ const ProcGen = preload("res://scenes/proc_gen.tscn")
 
 
 func _ready():
+	MusicManager.menu_music.play()
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _on_new_game_button_down():
+	MusicManager.fade("out",MusicManager.menu_music)
 	get_tree().change_scene_to_packed(ProcGen)
 
 func _on_exit_button_down():
---
+++ code/music_manager.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+# this is for the fade function
+var audio_volume
+# value to change the audiostream player's volume by
+var fade_increment = 1
+
+signal fade_complete
+
+# node refs
+@onready var menu_music = $Menu
+@onready var main_theme_1 = $MainTheme1
+@onready var main_theme_2 = $MainTheme2
+
+# fades audiostreamplayers in or out
+func fade(in_or_out: String,audiostreamplayer,start_value = null):
+		if in_or_out == "in":
+			if start_value:
+				audiostreamplayer.volume_db = start_value
+			for i in 40:
+				await get_tree().create_timer(.1).timeout
+				audiostreamplayer.volume_db += fade_increment
+		elif in_or_out == "out":
+			for i in 40:
+				await get_tree().create_timer(.1).timeout
+				audiostreamplayer.volume_db -= fade_increment
+		else: push_error("Fade in type not selected. First argument, type, must be 'in' or 'out'.")
